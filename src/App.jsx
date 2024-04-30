5d9f4b7e4dda272f24bbbd180125100dfa5b3b8f
Harish Madhavan A <166358859+hmadhavan57@users.noreply.github.com>
2024-04-30 23:32:18 +0530

    feat:commenting the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
+// Importing necessary components from the components directory
 import {
-  Navbar,
-  Home,
-  About,
-  Teacher,
-  Contact,
-  Courses,
-  Footer,
+  Navbar,   // Navbar component for navigation
+  Home,     // Home component for the homepage
+  About,    // About component for information about the website
+  Teacher,  // Teacher component for teacher-related content
+  Contact,  // Contact component for contact information
+  Courses,  // Courses component for displaying available courses
+  Footer,   // Footer component for the bottom section of the website
 } from "./components/index";
 
+// Main App component
 function App() {
   return (
+    // Wrapper div with custom classes for styling
     <div className="font-Poppins bg-Solitude">
+    
       <Navbar />
+
       <Home />
+    
       <About />
+    
       <Courses />
+     
       <Teacher />
+     
       <Contact />
+     
       <Footer />
     </div>
   );
 }
 
+// Exporting the App component as the default export
 export default App;
